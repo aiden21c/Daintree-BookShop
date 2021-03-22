c52b0b08fe7f086ca9703de5177f2a328aabd958
aiden21c <aiden21con@gmail.com>
2021-03-23 00:08:38 +1100

    Completed utilities test file

--- src/test/java/com/mycompany/utilities/UtilitiesTest.java
+++ src/test/java/com/mycompany/utilities/UtilitiesTest.java
@@ -4,21 +4,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+@Testable
 class UtilitiesTest {
 
 	@Test
-	void testGetBoolValue() {
-		fail("Not yet implemented");
+	@DisplayName("booleanShouldBeDerivedFromString")
+	public void testGetBoolValue() {
+		assertEquals(true, utilities.getBoolValue("yes"),
+		"Should return boolean value equal to string");
+
+		assertEquals(false, utilities.getBoolValue("nO"),
+		"Should return boolean value equal to string");
 	}
 
 	@Test
+	@DisplayName("booleanShouldBeValidatedFromString")
 	void testValidBool() {
-		fail("Not yet implemented");
+		assertEquals(true, utilities.validBool("YES"),
+		"Should return true if string can be mapped to boolean");
+
+		assertEquals(false, utilities.validBool("nothanks"),
+		"Should return true if string can be mapped to boolean");
 	}
 
 	@Test
+	@DisplayName("stringShouldBeIntegerBetweenSpecificRange")
 	void testValidInt() {
-		fail("Not yet implemented");
+		assertEquals(true, utilities.validInt("4", 0, 5),
+		"Should return true if the string is an int and lies between the range");
+
+		assertEquals(false, utilities.validInt("7", 0, 4),
+		"Should return true if the string is an int and lies between the range");
+
+		assertEquals(false, utilities.validInt("four", 0, 5),
+		"Should return true if the string is an int and lies between the range");
 	}
 
 }
